e135e917ba6a78ac2fe6cd31b101cae99de59c53
Maciej Rewera <22579925+MaciejRewera@users.noreply.github.com>
2021-01-22 20:09:17 +0000

    BAU Upgrade Play framework to version 2.7.9

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,14 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-backend-play-26"  % "3.3.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo"       % "7.31.0-play-26",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-27"  % "3.3.0",
+    "uk.gov.hmrc" %% "simple-reactivemongo"       % "7.31.0-play-27",
     "uk.gov.hmrc" %% "wco-dec"                    % "0.35.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"           %%  "scalatest"           % "3.0.9"   % "test",
-    "org.scalatestplus.play"  %%  "scalatestplus-play"  % "3.1.2"   % "test",
+    "org.scalatestplus.play"  %%  "scalatestplus-play"  % "4.0.3"   % "test",
     "org.pegdown"             %   "pegdown"             % "1.6.0"   % "test",
     "org.mockito"             %   "mockito-core"        % "3.5.7"   % "test"
   )
