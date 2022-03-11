85a30521dcb6b02caccf964e70031f53a05345d5
Tim <glidester@gmail.com>
2022-03-11 11:30:26 +0000

    CEDS-3784 Increase max size of XML allowed
    Allowed up to 100MB in size

--- app/uk/gov/hmrc/customs/declarations/stub/controllers/DeclarationStubController.scala
+++ app/uk/gov/hmrc/customs/declarations/stub/controllers/DeclarationStubController.scala
@@ -68,7 +68,7 @@ class DeclarationStubController @Inject()(
 
   logger.warn(s"Schema Validation enabled? ${schemaValidationConfig.isEnabled}")
 
-  def submit(): Action[NodeSeq] = Action.async(parse.xml) { implicit request =>
+  def submit(): Action[NodeSeq] = Action.async(parse.xml(maxLength = 1024 * 100000)) { implicit request =>
     validateHeaders() { headers =>
       authenticate(headers) { client =>
         validatePayload(submitSchemas) { meta =>
